3c108e392d8c0aca99bf9202f73ad3e18a92916c
Binh <haduybinh55555@gmail.com>
2025-10-23 04:13:54 +0700

    billing grpc service

--- billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
+++ billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
@@ -1,6 +1,33 @@
 package com.pm.billingservice.grpc;
 
-import billing.BillingServiceGrpc;
+import billing.BillingRequest;
+import billing.BillingResponse;
+import billing.BillingServiceGrpc.BillingServiceImplBase;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class BillingGrpcService extends BillingServiceGrpc.BillingServiceImplBase {
+@GrpcService
+public class BillingGrpcService extends BillingServiceImplBase {
+
+    private static final Logger log = LoggerFactory.getLogger(BillingGrpcService.class);
+
+    @Override
+    public void createBillingAccount(BillingRequest billingRequest,
+                                     StreamObserver<BillingResponse> responseStreamObserver){
+
+        log.info("createBillingAccount request received {}", billingRequest.toString());
+
+        String accountId = "ACC-" + billingRequest.getPatientId();
+
+        BillingResponse response = BillingResponse.newBuilder()
+                .setAccountId(accountId)
+                .setStatus("SUCCESS")
+                .build();
+
+        // Gửi kết quả về client
+        responseStreamObserver.onNext(response);
+        responseStreamObserver.onCompleted();
+    }
 }
